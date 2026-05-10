a94ceb5b9c9ec95f2657b9cef4ea0d086ed535b9
abella6710-create <abella6710@gmail.com>
2026-05-10 11:06:29 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "aaa-shop-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/logo.png",
+  "/manifest.json"
+];
+
+// Install service worker
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Serve cached content
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
