d7658bcfb93a713809915d6f046a5cdf375c4a78
abella6710-create <abella6710@gmail.com>
2026-05-10 14:56:02 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,17 @@
 const CACHE_NAME = "aaashop-v1";
 
+const urls = [
+  "index.html",
+  "manifest.json",
+  "logo.png",
+  "about.html",
+  "contact.html"
+];
+
 self.addEventListener("install", e=>{
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache=>{
-      return cache.addAll(["index.html","manifest.json"]);
+      return cache.addAll(urls);
     })
   );
 });
